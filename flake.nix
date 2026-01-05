0e598edf5013841e269a1c59e7d2418c6e8d4b45
Guillaume Friloux <gfriloux@datasolution.fr>
2026-01-05 10:01:40 +0100

    feat(all): first version
    first version
    
    Signed-off-by: Guillaume Friloux <gfriloux@datasolution.fr>

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    snowfall-lib = {
+      url = "github:snowfallorg/lib";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    flake-utils.url = "github:numtide/flake-utils";
+    home-manager = {
+      url = "github:nix-community/home-manager/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+  outputs = inputs:
+    inputs.snowfall-lib.mkFlake {
+      inherit inputs;
+      systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      src = ./.;
+
+      snowfall = {
+        namespace = "television-ssh";
+      };
+    };
+
+}
---
+++ modules/home/television-ssh/default.nix
@@ -0,0 +1,18 @@
+{ config, lib, pkgs, ... }:
+
+let
+  cfg = config.television-ssh.default;
+in
+{
+  options.television-ssh.hm = {
+    enable = lib.mkEnableOption "Enable television-ssh home-manager modules globally";
+  };
+
+  config = lib.mkIf cfg.enable {
+    programs.television = {
+      enable = true;
+    };
+
+    xdg.configFile."television/cable/ssh.toml".source = ../../../ssh.toml;
+  };
+}
